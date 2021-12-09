769ee87630a26bdb0e4ad35b890e39179ce2a6cb
n7560125 <95419045+n7560125@users.noreply.github.com>
2021-12-09 19:56:45 +0800

    add TankPlayer script.

---
+++ Assets/Scripts/TankPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Tanks
+{
+    public class TankPlayer : MonoBehaviour
+    {
+        PhotonView photonView;
+        //Reference to tank's movement script, used to disable and enable control.
+        private Complete.TankMovement m_Movement;
+        //Reference to tank's shooting script, used to disable and enable control.
+        private Complete.TankShooting m_Shooting;
+        // Start is called before the first frame update
+        void Awake()
+        {
+            m_Movement = GetComponent<Complete.TankMovement>();
+            m_Shooting = GetComponent<Complete.TankShooting>();
+            if (!photonView.IsMine)
+            {
+                m_Movement.enabled = false;
+                m_Shooting.enabled = false;
+                enabled = false;
+            }
+        }
+    }
+}
+
